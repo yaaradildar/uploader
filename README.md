0b7cebff3b1229cf55dce028711c86a87de28d66
yaaradildar <yaaradildaar@gmail.com>
2024-12-02 11:05:58 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# uploader
+## Deploy To heroku 
+
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/nothing/txt)
---
+++ vars.py
@@ -0,0 +1,9 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+from os import environ
+
+API_ID = int(environ.get("API_ID", "22801364"))
+API_HASH = environ.get("API_HASH", "38fd13fcb81f612aa703fa63d7dffec8")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7405263858:AAEGuzhvjmuXJAnQCyRhi3GiJHYxW7jhEZQ")
